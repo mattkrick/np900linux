8e4bb1e2e5dabf9d84051b25f6ec138cce72ee54
Matt Krick <matt.krick@gmail.com>
2017-05-11 18:17:58 -0700

    add ws

--- np900startup.sh
+++ np900startup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-synclient PalmDetect=1 HorizTwoFingerScroll=1 TapButton3=2 VertEdgeScroll=0 HorizEdgeScroll=0 TapAndDragGesture=0 MaxTapMove=20 SingleTapTimeout=100
+synclient PalmDetect=1
+HorizTwoFingerScroll=1
+TapButton3=2
+VertEdgeScroll=0
+HorizEdgeScroll=0
+TapAndDragGesture=0
+MaxTapMove=20
+SingleTapTimeout=100
+VertScrollDelta=50
+HorizScrollDelta=50
 syndaemon -i 2 -d -t -K
 webstorm
-nmcli con up uuid b50cb447-750a-466f-a42a-6b8b13f3a6cd
+nmcli con up uuid "$(nmcli con | grep California | tr -s ' ' | cut -d' ' -f5)"
